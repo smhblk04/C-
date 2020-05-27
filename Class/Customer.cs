649f74e49036c091a3fb7fbb63f5f47a43f695a0
Semih Balki <semihbalki@sabanciuniv.edu>
2020-05-27 21:43:43 +0300

    Add files via upload

---
+++ Class/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.City = "Elazig";
+            customer.Id = 1;
+            customer.Fname = "Mahmut";
+            customer.Lname = "Yildirim";
+
+            Customer customer2 = new Customer { Id=2, City="Antep", Fname="Mustafa", Lname="Gunal"};
+            Console.WriteLine(customer2.Fname);
+            Console.ReadLine();
+        }
+    }
+}
