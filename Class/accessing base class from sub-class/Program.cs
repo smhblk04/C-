ba518038af3d392d1e3f079efedc186fca4d9353
Semih Balki <semihbalki@sabanciuniv.edu>
2020-05-29 21:55:07 +0300

    Add files via upload

---
+++ Class/accessing base class from sub-class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//How to access base class from sub-class? 
+namespace Constructor_injection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager("Telefon");
+            personManager.Add();
+            Console.ReadLine();
+        }
+    }
+
+    class BaseClass
+    {
+        private string _entity;
+        public BaseClass(string entity)
+        {
+            _entity = entity;
+        }
+        public void Message()
+        {
+            Console.WriteLine("{0} message", _entity);
+        }
+    }
+
+    class PersonManager : BaseClass
+    {
+        public PersonManager(string entity) : base(entity)
+        { }
+        public void Add()
+        {
+            Console.WriteLine("Added.");
+            Message();
+        }
+    }
+
+}
