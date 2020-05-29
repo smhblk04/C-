c3e118f0f7e8fca858a5a9561442ac13d01eb7f2
Semih Balki <semihbalki@sabanciuniv.edu>
2020-05-29 19:31:13 +0300

    Add files via upload

---
+++ Class/How to use a public class in a different project/Constructors/obj/Debug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person persons = new Person { Fname = "Ela"};
+            Console.WriteLine(persons.Fname);
+        }
+    }
+}
